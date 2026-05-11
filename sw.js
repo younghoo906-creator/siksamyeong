b4fb6726c086e208b7e2c118e92e26844df027a5
younghoo906-creator <younghoo906@gmail.com>
2026-05-12 00:22:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'siksamyeong-v1';
+const urlsToCache = [
+  '/siksamyeong/',
+  '/siksamyeong/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
